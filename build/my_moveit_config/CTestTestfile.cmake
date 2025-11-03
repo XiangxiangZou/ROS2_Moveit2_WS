b69502390b1efcc1dfe3e7e92e9607c1b1346e2b
ZouXiangxiang <438405295@qq.com>
2025-11-03 19:55:37 +0800

    modefy

---
+++ build/my_moveit_config/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/zouxx/MyData/ROS2_Moveit2_WS/src/my_moveit_config
+# Build directory: /home/zouxx/MyData/ROS2_Moveit2_WS/build/my_moveit_config
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
