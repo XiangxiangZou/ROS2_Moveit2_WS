7142878a1a2587c7ec0f544f76aafe3a3b62c8b5
ZouXiangxiang <438405295@qq.com>
2025-11-07 13:09:48 +0800

    add gripper subscribe

---
+++ build/my_robot_commander_cpp/CMakeFiles/robot_commander.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/robot_commander.dir/src/robot_commander.cpp.o"
+  "CMakeFiles/robot_commander.dir/src/robot_commander.cpp.o.d"
+  "robot_commander"
+  "robot_commander.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/robot_commander.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- build/my_robot_commander_cpp/cmake_install.cmake
+++ build/my_robot_commander_cpp/cmake_install.cmake
@@ -67,6 +67,30 @@ if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT
 endif()
 
 if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/my_robot_commander_cpp/robot_commander" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/my_robot_commander_cpp/robot_commander")
+    file(RPATH_CHECK
+         FILE "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/my_robot_commander_cpp/robot_commander"
+         RPATH "")
+  endif()
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/my_robot_commander_cpp" TYPE EXECUTABLE FILES "/home/zouxx/MyData/ROS2_Moveit2_WS/build/my_robot_commander_cpp/robot_commander")
+  if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/my_robot_commander_cpp/robot_commander" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/my_robot_commander_cpp/robot_commander")
+    file(RPATH_CHANGE
+         FILE "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/my_robot_commander_cpp/robot_commander"
+         OLD_RPATH "/opt/ros/jazzy/lib:/opt/ros/jazzy/lib/x86_64-linux-gnu:"
+         NEW_RPATH "")
+    if(CMAKE_INSTALL_DO_STRIP)
+      execute_process(COMMAND "/usr/bin/strip" "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/my_robot_commander_cpp/robot_commander")
+    endif()
+  endif()
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  include("/home/zouxx/MyData/ROS2_Moveit2_WS/build/my_robot_commander_cpp/CMakeFiles/robot_commander.dir/install-cxx-module-bmi-noconfig.cmake" OPTIONAL)
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
   file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/ament_index/resource_index/package_run_dependencies" TYPE FILE FILES "/home/zouxx/MyData/ROS2_Moveit2_WS/build/my_robot_commander_cpp/ament_cmake_index/share/ament_index/resource_index/package_run_dependencies/my_robot_commander_cpp")
 endif()
 
